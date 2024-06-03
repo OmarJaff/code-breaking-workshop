effb242e91793206d665d0acc8db159b37e478b8
OmarJaff <me@omarjaff.com>
2024-06-03 13:07:13 +0300

    explode the message by spaces and wrap them with a div for styling

--- resources/views/livewire/create-code.blade.php
+++ resources/views/livewire/create-code.blade.php
@@ -1,8 +1,17 @@
-<div class="w-full flex justify-center mt-6">
+<div class="w-full flex flex-col items-center mt-6">
     <form action="">
-        <textarea wire:model.live="message" class="rounded-lg bg-white/20 text-white text-lg p-4"  name="" id="" cols="30" rows="10"></textarea>
-
-        @dd($this->letters());
+        <textarea autofocus placeholder="write your message..." wire:model.live="message" class="rounded-lg bg-white/20 text-white text-lg p-4"  name="" id="" cols="30" rows="10"></textarea>
     </form>
+     <div class="text-white mt-8 flex gap-8">
+        @foreach(explode(' ', $message) as $word)
+            <div class="word">
+        @foreach(str_split($word) as $character)
+            <span class="material-symbols-outlined character">
+                {{$this->letters[strtolower($character)] ?? $character }}
+            </span>
+        @endforeach
+            </div>
+        @endforeach
+    </div>
 
 </div>
