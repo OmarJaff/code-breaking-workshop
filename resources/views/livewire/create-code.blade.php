b837b302e7a32049fb944f866ae275197f649ed7
OmarJaff <me@omarjaff.com>
2024-06-03 12:34:59 +0300

    latest

--- resources/views/livewire/create-code.blade.php
+++ resources/views/livewire/create-code.blade.php
@@ -1,16 +1,8 @@
-<section class="px-6">
-    <div class="container mx-auto ">
-        <div class="flex w-full justify-center my-8">
-        <img src="/logo.svg"   alt="code breaker logo">
-        </div>
-    </div>
-    <div class="w-full flex justify-center mt-6">
-        <form action="">
-            <textarea wire:model.live="message" class="rounded-lg bg-white/20 text-white text-lg p-4"  name="" id="" cols="30" rows="10"></textarea>
-            <div class="text-white">
-                {{$message}}
-            </div>
-        </form>
+<div class="w-full flex justify-center mt-6">
+    <form action="">
+        <textarea wire:model.live="message" class="rounded-lg bg-white/20 text-white text-lg p-4"  name="" id="" cols="30" rows="10"></textarea>
 
-    </div>
-</section>
+        @dd($this->letters());
+    </form>
+
+</div>
