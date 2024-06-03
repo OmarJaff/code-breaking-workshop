f1d38bdc04b13c4d86336d68158b3a57daa7797b
OmarJaff <me@omarjaff.com>
2024-06-03 13:06:23 +0300

    returning a collection of mapped letters with the symbols

--- app/Livewire/CreateCode.php
+++ app/Livewire/CreateCode.php
@@ -2,19 +2,22 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class CreateCode extends Component
 {
     public $message;
+
     protected array $symbols = [
        'search', 'home' , 'favorite', 'star', 'check', 'key', 'deployed_code', 'public', 'sentiment_satisfied' , 'rocket_launch',
        'partly_cloudy_day',
         'sentiment_extremely_dissatisfied', 'wind_power','elderly_woman','eyeglasses','barefoot','visibility',
-        'brush','auto_stories','shopping_cart','local_shipping','flight','sailing','bike_lane','sports_esports,',
+        'brush','auto_stories','shopping_cart','local_shipping','flight','sailing','bike_lane','sports_esports',
         'sports_soccer'
     ] ;
 
+    #[Computed(persist: true)]
     public function letters () : array
     {
          shuffle($this->symbols);
