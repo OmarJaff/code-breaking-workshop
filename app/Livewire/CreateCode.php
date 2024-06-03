15c3305d51ec43ef9680c3e72bcc691e21bf2954
OmarJaff <me@omarjaff.com>
2024-06-03 12:33:48 +0300

    Wrote the logic for mapping the letters with symbols

--- app/Livewire/CreateCode.php
+++ app/Livewire/CreateCode.php
@@ -7,6 +7,19 @@ use Livewire\Component;
 class CreateCode extends Component
 {
     public $message;
+    protected array $symbols = [
+       'search', 'home' , 'favorite', 'star', 'check', 'key', 'deployed_code', 'public', 'sentiment_satisfied' , 'rocket_launch',
+       'partly_cloudy_day',
+        'sentiment_extremely_dissatisfied', 'wind_power','elderly_woman','eyeglasses','barefoot','visibility',
+        'brush','auto_stories','shopping_cart','local_shipping','flight','sailing','bike_lane','sports_esports,',
+        'sports_soccer'
+    ] ;
+
+    public function letters () : array
+    {
+         shuffle($this->symbols);
+         return collect(range('a' , 'z'))->combine($this->symbols)->toArray();
+    }
 
     public function render()
     {
