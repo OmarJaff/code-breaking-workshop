0a4f8d960c2bc0ee1932e4a6077e8c74ae0ec030
OmarJaff <me@omarjaff.com>
2024-06-03 13:17:44 +0300

    Extracted message convertor into separate component

--- resources/views/livewire/create-code.blade.php
+++ resources/views/livewire/create-code.blade.php
@@ -2,16 +2,6 @@
     <form action="">
         <textarea autofocus placeholder="write your message..." wire:model.live="message" class="rounded-lg bg-white/20 text-white text-lg p-4"  name="" id="" cols="30" rows="10"></textarea>
     </form>
-     <div class="text-white mt-8 flex gap-8">
-        @foreach(explode(' ', $message) as $word)
-            <div class="word">
-        @foreach(str_split($word) as $character)
-            <span class="material-symbols-outlined character">
-                {{$this->letters[strtolower($character)] ?? $character }}
-            </span>
-        @endforeach
-            </div>
-        @endforeach
-    </div>
+    <x-code :message="$message" :letters="$this->letters"></x-code>
 
 </div>
