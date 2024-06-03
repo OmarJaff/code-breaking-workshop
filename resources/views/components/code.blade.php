8c92dafd9e5cc52e26e23b73fcb42fff36c3a878
OmarJaff <me@omarjaff.com>
2024-06-03 16:11:44 +0300

    latest

--- resources/views/components/code.blade.php
+++ resources/views/components/code.blade.php
@@ -1,12 +1,17 @@
 @props(['message','letters'])
 
- <div class="text-white flex gap-8 ">
+ <div class="text-white flex gap-8 flex-wrap">
     @foreach(explode(' ', $message) as $word)
-        <div class="word">
+        <div class="word  gap-x-1 flex justify-center">
             @foreach(str_split($word) as $character)
+                <div class="flex flex-col items-center">
                <x-material-font>
                 {{$this->letters[strtolower($character)] ?? $character }}
                </x-material-font>
+                <div class="bg-gray-200 h-12 w-12 rounded-lg mt-1">
+
+                </div>
+                </div>
             @endforeach
         </div>
     @endforeach
